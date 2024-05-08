d58d9bb7b05d3f212b68bf0279bae9377708e8b6
Danylo Shlapak <geografy2314@gmail.com>
2024-05-08 12:27:04 +0300

    init

---
+++ eventify-spring-boot/src/main/java/com/project/eventifyspringboot/EventifySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventifyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventifySpringBootApplication.class, args);
+    }
+
+}
---
+++ eventify-spring-boot/src/test/java/com/project/eventifyspringboot/EventifySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventifyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
