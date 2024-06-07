0348469d66c74aeba1a543f5dcf44eb1ce864231
mx_lshk <max.loshak.i@gmail.com>
2024-06-07 14:16:39 +0300

    input on enter

--- react-front/src/components/forms/CommentInputForm.tsx
+++ react-front/src/components/forms/CommentInputForm.tsx
@@ -1,6 +1,7 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { getUserId } from "../../auth";
 import { Message } from "../../interfaces/MessageInterface";
+import { Form } from "react-router-dom";
 
 interface CommentInputFormProps {
     chatId: string;
@@ -8,29 +9,30 @@ interface CommentInputFormProps {
 }
 
 function CommentInputForm({ chatId, onSubmit }: CommentInputFormProps) {
+    const [messageContent, setMessageContent] = useState("");
     const userId = getUserId();
 
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        const form = event.currentTarget;
-        const formData = new FormData(form);
 
+        if (!messageContent.trim()) return;
         const date = new Date();
 
         const isoDateString = date.toISOString().replace('Z', '');
 
         const message = {
             senderId: userId,
-            content: formData.get('message'),
+            content: messageContent,
             sendTime: isoDateString,
         } as Message;
         console.log('Message:', message);
         onSubmit(chatId, message);
+        setMessageContent("");
     }
 
     return (
 
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
             <label htmlFor="chat" className="sr-only">Your message</label>
             <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50/40 dark:bg-gray-700">
                 <button type="button" className="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
@@ -39,7 +41,14 @@ function CommentInputForm({ chatId, onSubmit }: CommentInputFormProps) {
                     </svg>
                     <span className="sr-only">Add emoji</span>
                 </button>
-                <textarea id="chat" name="message" rows={1} className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Your message..."></textarea>
+                <input
+                    id="chat"
+                    name="message"
+                    value={messageContent}
+                    onChange={(e) => setMessageContent(e.target.value)}
+                    className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                    placeholder="Your message..."
+                ></input>
                 <button type="submit" className="inline-flex justify-center p-2 text-green-600 rounded-full cursor-pointer hover:bg-green-100 dark:text-green-500 dark:hover:bg-gray-600">
                     <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                         <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
@@ -47,7 +56,7 @@ function CommentInputForm({ chatId, onSubmit }: CommentInputFormProps) {
                     <span className="sr-only">Send message</span>
                 </button>
             </div>
-        </form>
+        </Form>
     )
 }
 
