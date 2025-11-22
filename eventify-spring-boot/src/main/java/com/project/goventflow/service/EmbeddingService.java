b441e89ba6bbbae5225391faf849ee6d072420f7
DoctorKaufman <andrey.krasovskyy@gmail.com>
2025-11-22 11:43:47 +0200

    Introduced @RequiredArgsConstructor.

--- eventify-spring-boot/src/main/java/com/project/goventflow/service/EmbeddingService.java
+++ eventify-spring-boot/src/main/java/com/project/goventflow/service/EmbeddingService.java
@@ -1,16 +1,14 @@
 package com.project.goventflow.service;
 
 import com.project.goventflow.domain.entity.Event;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.vertexai.embedding.text.VertexAiTextEmbeddingModel;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmbeddingService {
     private final VertexAiTextEmbeddingModel embeddingModel;
-
-    public EmbeddingService(VertexAiTextEmbeddingModel embeddingModel) {
-        this.embeddingModel = embeddingModel;
-    }
 
     public float[] embedText(String text) {
         return embeddingModel.embed(text);
